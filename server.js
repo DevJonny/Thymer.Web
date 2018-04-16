ed2b7f22e0a56c5aebc0e882b8f7ed71826d5242
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2018-04-16 17:46:13 +0100

    Updates server.js to point to /docs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/docs'));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/dist/index.html')
