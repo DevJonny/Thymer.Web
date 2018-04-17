41f9f1594cba41f9822b51c9130bed74e45eccf2
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2018-04-17 18:55:55 +0100

    Fixes server not starting up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/docs'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/dist/index.html')
